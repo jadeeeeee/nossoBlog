b17b48a68e193edd65840c8de5cfaad4f8cd2e05
Jade e Manu <jbeatriz.borborema12.@gmail.com>
2019-09-19 08:14:34 -0300

    html adicionada

--- nosso_site/nossoBlog.py
+++ nosso_site/nossoBlog.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route(/"/")
+@app.route("/")
 def index():
     return "Pagina inicial"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
